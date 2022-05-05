632c76d9d23393f4282346cfac5c05e218fc5ef5
jurandir moura <jurandirmoura40@gmail.com>
2022-05-05 20:31:10 -0300

    mapeando as minhas entidades com JPA

--- src/main/java/com/dio/santander/bankline/api/model/Conta.java
+++ src/main/java/com/dio/santander/bankline/api/model/Conta.java
@@ -1,8 +1,17 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Conta {
+	
+	@Column(name = "conta_numero")
 	private long numero;
+	
+	@Column(name = "conta_saldo")
 	private double saldo;
+	
 	public long getNumero() {
 		return numero;
 	}
